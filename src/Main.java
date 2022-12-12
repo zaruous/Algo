5f5b011bdbf9965d80d0b44070eb9112c1347085
zaruous <zaruous@naver.com>
2022-12-13 00:34:52 +0900

    머리맞대기

--- src/Main.java
+++ src/Main.java
@@ -15,50 +15,101 @@ public class Main {
 	private static StringTokenizer sc;
 	private static int N;
 	private static int W;
-	private static char[] arr;
+	private static int[] arr;
 	private static int M;
 	private static int R;
 	private static int K;
 
 	public static void main(String[] args) throws IOException {
 
-		N = Integer.parseInt(br.readLine());
-		// sc = new StringTokenizer(br.readLine());
+		sc = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(sc.nextToken());
+		T = Integer.parseInt(sc.nextToken());
+
+		sc = new StringTokenizer(br.readLine());
+
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(sc.nextToken());
+		}
+
+		int teamMaxMin = Integer.MAX_VALUE;
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+
+		for (int i = 0; i < T; i++) {
+			sum += arr[i];
+		}
+		min = sum;
+
+		int div = N / T + (N % T == 0 ? 0 : 1);
+		int s = 0, e = div;
+
+		int mins = 0, mine = 0;
+		for (int i = 0; i < N; i++) {
+
+			int a = arr[s % N];
+			int b = arr[e % N];
+
+			sum -= a;
+			sum += b;
+
+			if (Integer.min(min, sum) == sum) {
+				mins = s % N;
+				mine = e % N;
+				min = Integer.min(min, sum);
+			}
+
+			// teamMaxMin = Integer.min(max, teamMaxMin);
+			System.out.println(min + " a  : " + a + " b : " + b);
+
+			s++;
+			e++;
+		}
+		System.out.println(aa(mins, mine));
+
+	}
+
+	static int aa(int ss, int ee) {
+		int sum = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < T; i++) {
+			sum += arr[i];
+		}
+		max = sum;
+
+		int div = N / T + (N % T == 0 ? 0 : 1);
+		int s = 0, e = div;
 
 		for (int i = 0; i < N; i++) {
-			arr = br.readLine().toCharArray();
-			int cnt = binarySearch('#', 0, arr.length);
-			System.out.println((cnt * 100) / arr.length);
-		}
 
+			if ((e <= ss || s  <= ee )) {
+				s++;
+				e++;
+				continue;
+			}
+
+			int a = arr[s % N];
+			int b = arr[e % N];
+
+			sum -= a;
+			sum += b;
+
+			// if(Integer.max(max, sum) == sum)
+			// {
+			//
+			// }
+			max = Integer.max(max, sum);
+			// teamMaxMin = Integer.min(max, teamMaxMin);
+			System.out.println("v " + max + " a  : " + a + " b : " + b);
+
+			s++;
+			e++;
+
+		}
+		System.out.println();
+		return max;
 	}
 
-	static int sw = Integer.MAX_VALUE;
-
-	/**
-	 * @작성자 : KYJ (callakrsos@naver.com)
-	 * @작성일 : 2022. 12. 12.
-	 * @param srch
-	 * @param s
-	 * @param e
-	 * @return
-	 */
-	private static int binarySearch(char srch, int s, int e) {
-
-		if (s > e) {
-			return e;
-		}
-
-		int c = 0;
-		int mid = (s + e) / 2;
-
-		if (arr[mid] == srch) {
-			sw = Integer.max(sw, mid);
-			return binarySearch(srch, mid + 1, e);
-		} else {
-			sw = Integer.max(sw, mid);
-			return binarySearch(srch, mid - 1, e);
-		}
-
-	}
 }
